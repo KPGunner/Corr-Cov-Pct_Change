b0df8748aaa27b122d3862181f40a4f7447a5ce1
Gunn <31373828+KPGunner@users.noreply.github.com>
2018-03-26 21:20:19 -0400

    Update CorrWithPctChange.py

--- CorrWithPctChange.py
+++ CorrWithPctChange.py
@@ -4,21 +4,24 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import csv
 
-#GFC#
+##THIS IS VERY MUCH A WORK IN PROGRESS. PLEASE FORGIVE ME##
+
+#GFC Peak to trough dates#
 ##start = dt.datetime(2007, 10, 9)
 ##end = dt.datetime(2009, 3, 9)
 
-###Custom#
-start = dt.datetime(2018, 1, 1)
+#Custom#
+start = dt.datetime(2017, 1, 1)
 end = dt.datetime(2018, 3, 24)
 
 
-#Sector Tickers#
+#SPDR Main Sector Tickers#
 #tickers = ['XLV', 'XLU', 'XLE', 'XLB', 'XLI', 'XLP', 'XLRE', 'XLF', 'XLY', 'XLK', 'AGG', 'SPY']
 
 #Defense Tickers#
 #tickers = ['BA', 'UTX', 'LMT', 'GD', 'RTN', 'COL', 'LLL', 'TDG', 'HII', 'SPR', 'XAR', 'ITA', 'SPY', 'NOC']
 
+#Custom Tickers#
 tickers = ['MTUM', 'VLUE', 'USMV', 'SPY', 'EEM', 'AGG', 'GLD']
 
 #Start to pull data#
@@ -27,25 +30,23 @@ df = web.DataReader(tickers, 'yahoo', start, end)
 price = df['Adj Close'] #Use 'Adj Close' to account for dividends and splits.#
 #price= df['Close'] #Use just 'Close' when calculating inside periods not going to present#
 
+dev = price.std()
+
 returns = price.pct_change()
-ret = price.rolling(252).mean()
+ret = price.rolling(252).mean() #Annual rolling percent change#
 
 #Calculate Percent Change#
-
 #Manipulate data to pull change from start to end. Still working out how to best accomplish this#
+
 df1 = price.iloc[[0, -1]]
-print(df1)
+#print(df1)
 
 pct = df1.pct_change()*100
-
-print(pct)
 #pct.to_csv('da.csv')
 
 df2 = df1.iloc[[0, -1]] = df1.iloc[[-1, 0]]
 print(df2)
 change = df2.pct_change()*100
-
-print(change)
 #change.to_csv('Pct.csv')
 
 
@@ -67,8 +68,7 @@ corr = returns.rolling(5, min_periods=1).corr(price)*100
 x = returns.cov()
 
 #print (returns)
-print (z)
-print (x)
+
 #print(corr)
 
 
@@ -85,3 +85,17 @@ print (x)
 
 ##corr.plot()
 ##plt.show()
+
+print ('Standard Deviation\n',(dev))
+print ('Covariance\n', (x))
+print ('Correlation\n', (z))
+#print (pct)
+print ('Percent Returns\n', (change))
+
+##Combine all of them into one dataframe#
+##df4 = dev
+##df5 = pct
+##df6 = change
+##df7 = z
+##df8 = x
+##df9 = 
